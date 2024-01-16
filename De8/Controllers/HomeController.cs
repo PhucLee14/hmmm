0f53960d2cbcb33a0c26d2bec62ebb895dfa81c7
PhucLee14 <lhphucth14@gmail.com>
2024-01-16 08:01:02 +0700

    cm

---
+++ De8/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using De8.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data.Entity;
+using System.Web;
+using System.Web.Mvc;
+using System.Net;
+
+namespace De8.Controllers
+{
+    public class HomeController : Controller
+    {
+        private QLBanChauCanhEntities db = new QLBanChauCanhEntities();
+
+        // GET: SanPhams
+        public ActionResult Index()
+        {
+            var sanPhams = db.SanPhams.Include(s => s.PhanLoai).Include(s => s.PhanLoaiPhu);
+            ViewBag.PhanLoai = new SelectList(db.PhanLoaiPhus, "MaPhanLoaiPhu", "TenPhanLoaiPhu");
+            return View(sanPhams.ToList());
+        }
+
+        // GET: SanPhams/Details/5
+        public ActionResult ChiTietSanPham(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SanPham sanPham = db.SanPhams.Find(id);
+            if (sanPham == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sanPham);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
